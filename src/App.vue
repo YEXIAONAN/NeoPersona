8f9b8bdb86afc08f021fd59b452c48144ae54b48
YEXIAONAN <yexiaonanair@outlook.com>
2025-07-09 20:22:12 +0800

    fix

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,15 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
+import BgURL from './assets/bg.jpg'
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+
+
+
+<h1>NeoPersona - 本地数字人</h1>
+<h3>Your Private, Intelligent Digital Human - by yexiaonan</h3>
+
 </template>
 
 <style scoped>
@@ -26,5 +24,19 @@ import HelloWorld from './components/HelloWorld.vue'
 }
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
+}
+
+.image-wrapper {
+  width: 100%;
+  max-width: 600px; /* 最大宽度限制 */
+  margin: 0 auto; /* 居中显示 */
+}
+
+.responsive-img {
+  width: 100%;
+  height: auto; /* 保持原图比例 */
+  display: block;
+  border-radius: 8px; /* 可选：圆角 */
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.1); /* 可选：阴影 */
 }
 </style>
